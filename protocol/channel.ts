debdbee387b28dce0fca2b7cf9c7bc36b7a95f70
Synthetic Goop <code@synthetic-goop.com>
2024-06-03 16:47:16 +0800

    fix(channel): Fix socket message ordering issue where messages would be lost
    This would happen because channel socket would multiplex across each loop, resulting in lost messages as multiplexing clears all bufferered messages.
    
    Instead now the signal recv handle across loops shares one buffered socket ensuring no lost messages

--- protocol/channel.ts
+++ protocol/channel.ts
@@ -19,97 +19,103 @@ export function channel(
 		push: (value: CancellableResult<ArrayBuffer>) => void;
 	},
 ) {
-	task.poll((parentTask) => {
-		const task = parentTask.subtask();
-		const sessionSocket = socket.multiplex();
-		const { pull, push } = createHandle(task);
-		const sendAtomic = new Atomic(0xffff);
-		const initStep = sendAtomic.next;
+	const channelSocket = socket.multiplex();
 
-		const sigHandle = recv(
-			sessionSocket,
-			{
-				...proto,
-				type: "SIG",
-				filter: ({ buffer, proto }) => {
-					const result = match(initStep, buffer);
-					if (!result) return null;
+	task
+		.poll((parentTask) => {
+			const task = parentTask.subtask();
+			const { pull, push } = createHandle(task);
+			const sendAtomic = new Atomic(0xffff);
+			const initStep = sendAtomic.next;
 
-					if (result.buffer.byteLength !== 1) return null;
-					if (new DataView(result.buffer).getUint8(0) !== SIGNAL.INIT)
-						return null;
-
-					return { ...result, nonce: proto.nonce };
-				},
-			},
-			task.subtask(),
-		);
-
-		sigHandle.deadline(recvTimeoutMs, "sig init").then((estab) => {
-			if (typeof estab.reason === "string") {
-				task.cancel(estab.reason);
-				return estab;
-			}
-			const nonce = estab.value.nonce;
-
-			const termSocket = socket.multiplex();
-			const term = recv(
-				termSocket,
+			const sigHandle = recv(
+				channelSocket,
 				{
 					...proto,
-					nonce,
 					type: "SIG",
-					filter: ({ buffer }) => {
-						const result = match(null, buffer);
+					filter: ({ buffer, proto }) => {
+						const result = match(initStep, buffer);
 						if (!result) return null;
 
 						if (result.buffer.byteLength !== 1) return null;
-						if (new DataView(result.buffer).getUint8(0) !== SIGNAL.TERM)
+						if (new DataView(result.buffer).getUint8(0) !== SIGNAL.INIT)
 							return null;
-						return result;
+
+						return { ...result, nonce: proto.nonce };
 					},
 				},
-				task,
+				task.subtask(),
 			);
 
-			listen(
-				sessionSocket,
-				{ ...proto, nonce },
-				task,
-				sendAtomic.clone(),
-				recvTimeoutMs,
-				push,
-			);
+			sigHandle.deadline(recvTimeoutMs, "sig init").then((estab) => {
+				if (typeof estab.reason === "string") {
+					task.cancel(estab.reason);
+					return estab;
+				}
+				const nonce = estab.value.nonce;
 
-			send(
-				sessionSocket,
-				{ ...proto, type: "ACK", nonce },
-				brand(initStep, new Uint8Array(0).buffer),
-			);
+				const sessionSocket = channelSocket.multiplex();
+				const termSocket = channelSocket.multiplex();
+				const term = recv(
+					termSocket,
+					{
+						...proto,
+						nonce,
+						type: "SIG",
+						filter: ({ buffer }) => {
+							const result = match(null, buffer);
+							if (!result) return null;
 
-			const streamSocket = socket.multiplex();
-			const streamHandle = stream(
-				streamSocket,
-				{ ...proto, nonce },
-				task,
-				sendAtomic,
-				sendTimeoutMs,
-				pull,
-			);
+							if (result.buffer.byteLength !== 1) return null;
+							if (new DataView(result.buffer).getUint8(0) !== SIGNAL.TERM)
+								return null;
+							return result;
+						},
+					},
+					task,
+				);
 
-			streamHandle.then(() => task.cleanup("channel stream finished"));
+				listen(
+					sessionSocket,
+					{ ...proto, nonce },
+					task,
+					sendAtomic.clone(),
+					recvTimeoutMs,
+					push,
+				);
 
-			term.then(() => {
-				streamSocket.drop();
-				termSocket.drop();
-				sessionSocket.drop();
+				send(
+					sessionSocket,
+					{ ...proto, type: "ACK", nonce },
+					brand(initStep, new Uint8Array(0).buffer),
+				);
+
+				const streamSocket = socket.multiplex();
+				const streamHandle = stream(
+					streamSocket,
+					{ ...proto, nonce },
+					task,
+					sendAtomic,
+					sendTimeoutMs,
+					pull,
+				);
+
+				streamHandle.then(() => task.cleanup("channel stream finished"));
+
+				term.then(() => {
+					streamSocket.drop();
+					termSocket.drop();
+					sessionSocket.drop();
+				});
+
+				return term;
 			});
 
-			return term;
+			return sigHandle;
+		})
+		.finally(() => {
+			channelSocket.drop();
 		});
-
-		return sigHandle;
-	});
 
 	return task;
 }
